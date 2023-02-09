d4ca909e26ebc7ad763a36199644ad1cc207aec2
Wenguang <1458892835@qq.com>
2023-02-09 14:28:27 +0800

    开启代理对象的获取

--- src/main/java/com/hmdp/HmDianPingApplication.java
+++ src/main/java/com/hmdp/HmDianPingApplication.java
@@ -3,9 +3,14 @@ package com.hmdp;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @MapperScan("com.hmdp.mapper")
+@EnableAspectJAutoProxy(exposeProxy = true)
 @SpringBootApplication
+
 public class HmDianPingApplication {
 
     public static void main(String[] args) {
